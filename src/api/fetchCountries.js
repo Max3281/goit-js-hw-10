deeb98469fa226261e4ab7b714af6d89026c1e16
Max3281 <113448092+Max3281@users.noreply.github.com>
2023-03-04 12:24:25 +0300

    edit 2

--- src/api/fetchCountries.js
+++ src/api/fetchCountries.js
@@ -1,5 +1,10 @@
 export function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
-  );
+  ).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw new Error(res.statusText);
+  });
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import Notiflix from 'notiflix';
 import './css/styles.css';
-import { countriesList, countryDescription } from './template/template';
 import { fetchCountries } from './api/fetchCountries';
 
 const inputEl = document.querySelector('#search-box');
@@ -12,30 +11,28 @@ const DEBOUNCE_DELAY = 300;
 inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(evt) {
-  const inputValue = evt.target.value.trim();
+  const inputValue = evt.target.value;
+  if (inputValue === '') {
+    return;
+  }
+  clear();
   fetchCountries(inputValue)
-    .then(res => {
-      return res.json();
-    })
     .then(value => {
-      if (value.length >= 10) {
+      if (value.length > 10) {
         Notiflix.Notify.warning(
           'Too many matches found. Please enter a more specific name.'
         );
         return;
       }
-      clear();
-      const templateUnit = value.map(unit => {
-        console.log(unit);
-        if (value.length <= 1) {
-          createCountryDescription(unit);
-          return;
-        }
-        createCountriesList(unit);
-      });
+      if (value.length === 1) {
+        createDescriptionMarkup(value);
+        return;
+      }
+      createListMarkup(value);
     })
     .catch(error => {
       Notiflix.Notify.failure('Oops, there is no country with that name');
+      // Notiflix.Notify.failure(`${error}`);
       console.log(error);
     });
 }
@@ -44,10 +41,34 @@ function clear() {
   ulEl.innerHTML = '';
 }
 
-function createCountriesList(array) {
-  return ulEl.insertAdjacentHTML('beforeend', countriesList(array));
+function createListMarkup(array) {
+  let templateUnit = array
+    .map(unit => {
+      return `<li class="country-unit">
+  <svg width="25" height="25">
+  <use href="${unit.flags.svg}"></use>
+</svg>
+  <p>${unit.name}</p>
+  </li>`;
+    })
+    .join('');
+  return ulEl.insertAdjacentHTML('beforeend', templateUnit);
 }
 
-function createCountryDescription(array) {
-  return ulEl.insertAdjacentHTML('beforeend', countryDescription(array));
+function createDescriptionMarkup(array) {
+  let templateUnit = array
+    .map(unit => {
+      return `<h1 class = "country-header"><svg width="50" height="50">
+  <use href="${unit.flags.svg}"></use>
+</svg>${unit.name}</h1>
+  <ul class ="country-list">
+  <li class="description-list">Capital: ${unit.capital}</li>
+  <li class="description-list">Population: ${unit.population}</li>
+  <li class="description-list">Languages: ${unit.languages.map(
+    lang => lang.name
+  )}</li>
+  </ul>`;
+    })
+    .join('');
+  return ulEl.insertAdjacentHTML('beforeend', templateUnit);
 }
